9ec09cce8361449a058cf476c81c1916da1ede3f
unknown <Shellynguyen98@gmail.com>
2020-09-02 13:50:25 -0700

    update

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,6 +5,9 @@ const { join } = require('path')
 router.get('/todo', (req, res) => {
   res.sendFile(join(__dirname, '..', 'public', 'todo.html'))
 })
+router.get('/about', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'public', 'Aboutus.html'))
+})
 
 router.get('/*', (req, res) => {
   res.sendFile(join(__dirname, '..', 'public', 'index.html'))
